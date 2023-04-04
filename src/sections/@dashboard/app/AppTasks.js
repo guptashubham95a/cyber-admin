d19c0406de3be483d6636a6eb2695e062411c63e
guptashubham95a <guptashubham95a@gmail.com>
2023-04-04 13:15:39 +0530

    Added dynamic toDos using firebase.

--- src/sections/@dashboard/app/AppTasks.js
+++ src/sections/@dashboard/app/AppTasks.js
@@ -1,7 +1,10 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 // form
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller } from "react-hook-form";
+import db from "../../../firebase";
+import { auth } from "../../../firebase";
+
 // @mui
 import {
   Card,
@@ -13,9 +16,9 @@ import {
   IconButton,
   CardHeader,
   FormControlLabel,
-} from '@mui/material';
+} from "@mui/material";
 // components
-import Iconify from '../../../components/iconify';
+import Iconify from "../../../components/iconify";
 
 // ----------------------------------------------------------------------
 
@@ -26,9 +29,30 @@ AppTasks.propTypes = {
 };
 
 export default function AppTasks({ title, subheader, list, ...other }) {
+  const [todos, setTodos] = useState([]);
+  const getTodos = async () => {
+    const res = [];
+    // console.log("todos res iiiiiddd", auth.currentUser.uid);
+    await db
+      .collection("todos")
+      .where("branchId", "==", auth.currentUser.uid)
+      .get()
+      .then((todos) => {
+        todos.forEach((todo) => res.push(todo.data()));
+      });
+    console.log(res.length, "fetched from todos", res);
+    console.log("todos res", res);
+    console.log("todos-->");
+    setTodos(res);
+    return res;
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   const { control } = useForm({
     defaultValues: {
-      taskCompleted: ['2'],
+      taskCompleted: ["2"],
     },
   });
 
@@ -36,20 +60,22 @@ export default function AppTasks({ title, subheader, list, ...other }) {
     <Card {...other}>
       <CardHeader title={title} subheader={subheader} />
       <Controller
-        name="taskCompleted"
+        name='taskCompleted'
         control={control}
         render={({ field }) => {
           const onSelected = (task) =>
-            field.value.includes(task) ? field.value.filter((value) => value !== task) : [...field.value, task];
-
+            field.value.includes(task)
+              ? field.value.filter((value) => value !== task)
+              : [...field.value, task];
+          if (todos.length === 0) return;
           return (
             <>
-              {list.map((task) => (
+              {todos.map((todo) => (
                 <TaskItem
-                  key={task.id}
-                  task={task}
-                  checked={field.value.includes(task.id)}
-                  onChange={() => field.onChange(onSelected(task.id))}
+                  key={todo.todoId}
+                  todo={todo}
+                  checked={field.value.includes(todo.todoId)}
+                  onChange={() => field.onChange(onSelected(todo.todoId))}
                 />
               ))}
             </>
@@ -71,7 +97,7 @@ TaskItem.propTypes = {
   }),
 };
 
-function TaskItem({ task, checked, onChange }) {
+function TaskItem({ todo, checked, onChange }) {
   const [open, setOpen] = useState(null);
 
   const handleOpenMenu = (event) => {
@@ -84,82 +110,88 @@ function TaskItem({ task, checked, onChange }) {
 
   const handleMarkComplete = () => {
     handleCloseMenu();
-    console.log('MARK COMPLETE', task.id);
+    console.log("MARK COMPLETE", todo.todoId);
   };
 
   const handleShare = () => {
     handleCloseMenu();
-    console.log('SHARE', task.id);
+    console.log("SHARE", todo.todoId);
   };
 
   const handleEdit = () => {
     handleCloseMenu();
-    console.log('EDIT', task.id);
+    console.log("EDIT", todo.todoId);
   };
 
-  const handleDelete = () => {
+  const handleDelete = async () => {
     handleCloseMenu();
-    console.log('DELETE', task.id);
+    const res = await db.collection("todos").doc(todo.todoId).delete();
+    console.log("deleting from TaskItem", todo.todoId, res);
   };
 
   return (
     <Stack
-      direction="row"
+      direction='row'
       sx={{
         px: 2,
         py: 0.75,
         ...(checked && {
-          color: 'text.disabled',
-          textDecoration: 'line-through',
+          color: "text.disabled",
+          textDecoration: "line-through",
         }),
       }}
     >
       <FormControlLabel
         control={<Checkbox checked={checked} onChange={onChange} />}
-        label={task.label}
+        label={todo.task}
         sx={{ flexGrow: 1, m: 0 }}
       />
 
-      <IconButton size="large" color="inherit" sx={{ opacity: 0.48 }} onClick={handleOpenMenu}>
-        <Iconify icon={'eva:more-vertical-fill'} />
+      <IconButton
+        size='large'
+        color='inherit'
+        sx={{ opacity: 0.48 }}
+        onClick={handleOpenMenu}
+      >
+        <Iconify icon={"eva:more-vertical-fill"} />
       </IconButton>
 
       <Popover
         open={Boolean(open)}
         anchorEl={open}
         onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         PaperProps={{
           sx: {
             p: 1,
-            '& .MuiMenuItem-root': {
+            "& .MuiMenuItem-root": {
               px: 1,
-              typography: 'body2',
+              typography: "body2",
               borderRadius: 0.75,
             },
           },
         }}
       >
         <MenuItem onClick={handleMarkComplete}>
-          <Iconify icon={'eva:checkmark-circle-2-fill'} sx={{ mr: 2 }} />
+          <Iconify icon={"eva:checkmark-circle-2-fill"} sx={{ mr: 2 }} />
           Mark Complete
         </MenuItem>
 
         <MenuItem onClick={handleEdit}>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
+          <Iconify icon={"eva:edit-fill"} sx={{ mr: 2 }} />
           Edit
         </MenuItem>
 
         <MenuItem onClick={handleShare}>
-          <Iconify icon={'eva:share-fill'} sx={{ mr: 2 }} />
+          <Iconify icon={"eva:share-fill"} sx={{ mr: 2 }} />
           Share
         </MenuItem>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        <Divider sx={{ borderStyle: "dashed" }} />
 
-        <MenuItem onClick={handleDelete} sx={{ color: 'error.main' }}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
+        <MenuItem onClick={handleDelete} sx={{ color: "error.main" }}>
+          <Iconify icon={"eva:trash-2-outline"} sx={{ mr: 2 }} />
           Delete
         </MenuItem>
       </Popover>
